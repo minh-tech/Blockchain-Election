8b828a0c7d45e19ce2e6dd2a84a407e80f8a5bad
minh <hoangminh1612@gmail.com>
2020-02-07 21:00:21 -0800

    Cast votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,44 @@
 pragma solidity >=0.5.16;
 
 contract Election {
-  // Store candidate
-  // Read candidate
-  string public candidate;
-  // Constructor
-  function election() public {
-    candidate = "Candidate 1";
+  // Model candidate
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
   }
+  // Store voters
+  mapping(address => bool) public voters;
+
+  // Store candidates
+  mapping(uint => Candidate) public candidates;
+
+  // Store candidates count
+  uint public candidatesCount;
+
+  // Constructor
+  constructor() public {
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate(string memory _name) private {
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
+  function vote(uint _candidateId) public {
+    // require that they haven't voted before
+    require(!voters[msg.sender]);
+
+    // require a valid candidate
+    require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+    // record the voter has voted
+    voters[msg.sender] = true;
+
+    // Update candidate vote voteCount
+    candidates[_candidateId].voteCount++;
+  }
+
 }
